69b6fa5bec1a5490c35fdc509b91c86f045816b8
Asus <dhanashreehakke7@gmail.com>
2021-11-13 20:26:24 +0530

    Added: Shuffle the cards file is added

--- src/main/java/com/blp/workshopproblem/DeckOfCards.java
+++ src/main/java/com/blp/workshopproblem/DeckOfCards.java
@@ -4,6 +4,9 @@ package com.blp.workshopproblem;
  * Deck of cards program
  */
 public class DeckOfCards {
+    public static int[] deck;
+    public static String suit;
+    public static String rank;
 
     public static void main(String[] args) {
         System.out.println("Welcome to deck of cards");
@@ -11,15 +14,14 @@ public class DeckOfCards {
         Player person = new Player();
         person.numOfPlayer();
         person.sequence();
+        shuffleCards();
     }
 
     /**
      * Initialize deck by calling method.
      */
     private static void initializeCards() {
-        int[] deck = new int[52];
-        String suit;
-        String rank;
+        deck = new int[52];
         String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
         String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         for (int i = 1; i <= deck.length - 1; i++) {
@@ -30,6 +32,18 @@ public class DeckOfCards {
             suit = suits[deck[i] / 13];
             rank = ranks[deck[i] % 13];
             System.out.println(rank + " " + "of" + " " + suit);
+        }
+    }
+
+    /**
+     * Shuffle the cards for distribution
+     */
+    private static void shuffleCards() {
+        for (int i = 0; i < deck.length; i++) {
+            int index = (int) (Math.random() * deck.length);
+            int card = deck[i];
+            deck[i] = deck[index];
+            System.out.println("shuffled cards = " + deck[i]);
         }
     }
 }
